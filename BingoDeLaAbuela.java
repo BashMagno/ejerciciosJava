3ab0c02f719f999b0d48eac8331ac76b67c8204e
Alejandro Higuera Moreno <115299343+AlejandroHiguera@users.noreply.github.com>
2022-11-16 11:04:39 +0100

    Update BingoDeLaAbuela.java

--- BingoDeLaAbuela.java
+++ BingoDeLaAbuela.java
@@ -4,9 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 //Realizado por Alejandro Higuera 
 
-public class Bingo {
+public class Bingo 
+{
  
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int bombo = 0, aux, acertar;
         boolean ordenar = true;
         int[][] carton = new int[3][9];
@@ -21,21 +23,30 @@ public class Bingo {
         System.out.println("");
     }
  
-    public static void ordenar(int[][] carton) {
+    public static void ordenar(int[][] carton) 
+    {
         Random ran = new Random();
         int k = 0;
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[i].length; j++) {
-                if (i == 0) {
-                    if (j == 0) {
+        for (int i = 0; i < carton.length; i++) 
+        {
+            for (int j = 0; j < carton[i].length; j++) 
+            {
+                if (i == 0) 
+                {
+                    if (j == 0) 
+                    {
                     	carton[i][j] = ran.nextInt(7) + (10 * j) + 1;
-                    } else {
+                    } else 
+                    {
                     	carton[i][j] = ran.nextInt(8) + (10 * j);
                     }
-                } else {
-                    if (i == 1) {
+                } else 
+                {
+                    if (i == 1) 
+                    {
                         k = 0;
-                    } else if (i == 2) {
+                    } else if (i == 2) 
+                    {
                         k = 1;
                     }
                     carton[i][j] = ran.nextInt(9 + k) + (10 * j);
@@ -48,12 +59,15 @@ public class Bingo {
         carton[2][8]++;
     }
  
-    public static int bombos(int[][] carton) {
+    public static int bombos(int[][] carton) 
+    {
     	Random ran = new Random();
     	int support = ran.nextInt(90);
         int bombo = support;
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[i].length; j++) {
+        for (int i = 0; i < carton.length; i++) 
+        {
+            for (int j = 0; j < carton[i].length; j++) 
+            {
             	carton[0 + 1][0] = bombo + ran.nextInt(6);
             	carton[1][0] = bombo + ran.nextInt(6);
             	carton[2][1] = bombo + ran.nextInt(6);
@@ -71,30 +85,36 @@ public class Bingo {
         return bombo;
     }
  
-    public static void imprimirCuadro(int[][] carton) {
+    public static void imprimirCuadro(int[][] carton) 
+    {
         //Imprimir la interfaz del bingo
         System.out.println("Cartón del BINGO de las abuelas");
         System.out.println("------------------------------------");
         System.out.println("Bingo realizado por Alex HM");
         System.out.println("------------------------------------");
  
-        for (int i = 0; i < carton.length; i++) {
+        for (int i = 0; i < carton.length; i++) 
+        {
             System.out.println(""); 
-            for (int j = 0; j < carton[i].length; j++) {
+            for (int j = 0; j < carton[i].length; j++) 
+            {
                 System.out.printf("%3d ", carton[i][j]);
             }
         }
     }
  
-    public static int sorteig_Bingo(int[][] carton, int[] sorteo) {
+    public static int sorteig_Bingo(int[][] carton, int[] sorteo) 
+    {
         Scanner ranlat = new Scanner(System.in);
         Random ran = new Random();
         char opcio = ' ';
-        for (int i = 0; i < sorteo.length; i++) {
+        for (int i = 0; i < sorteo.length; i++) 
+        {
         	sorteo[i] = numsRepetits(sorteo);
         }
         //Imprimimos los numeros del bingo
-        for (int i = 0; i < sorteo.length; i++) {
+        for (int i = 0; i < sorteo.length; i++) 
+        {
             System.out.println("");
             System.out.println("");
             System.out.println("------------------------------------");
@@ -103,7 +123,8 @@ public class Bingo {
  
             do {
                 opcio = ranlat.next().charAt(0);
-                if (opcio != 's' && opcio != 'n') {
+                if (opcio != 's' && opcio != 'n') 
+                {
                 	
                     System.out.println("Introduce 's' o 'n'!");
                 }
@@ -112,13 +133,15 @@ public class Bingo {
             if (opcio == 's') {
                 System.out.println("");
  
-                if (comproBingo(carton) == true) {
+                if (comproBingo(carton) == true) 
+                {
                     System.out.println("BINGOOO!!!");
                     System.out.print("Numeros que han salido: ");
                     return i;
                 }
                 imprimirCuadro(carton);
-            } else if (opcio == 'n'){
+            } else if (opcio == 'n')
+            {
                 System.out.println("Hasta la proxima!!!!!!!");
                 System.out.print("Numeros que han salido: ");
                 return i;
@@ -129,14 +152,17 @@ public class Bingo {
         return sorteo.length;
     }
  
-    public static void mostrarUsados(int[] sorteo, int numSorteo) {
-        for (int i = 0; i <= numSorteo; i++) {
+    public static void mostrarUsados(int[] sorteo, int numSorteo) 
+    {
+        for (int i = 0; i <= numSorteo; i++) 
+        {
             System.out.print(sorteo[i] + ",");
         }
         System.out.println();
     }
  
-    public static int numsRepetits(int[] sorteo) {
+    public static int numsRepetits(int[] sorteo) 
+    {
  
         Random ran = new Random();
         int num;
@@ -145,8 +171,10 @@ public class Bingo {
         do {
         	repetir = false;
             num = ran.nextInt(90) + 1;
-            for (int i = 0; i < 90; i++) {
-                if (num == sorteo[i]) {
+            for (int i = 0; i < 90; i++) 
+            {
+                if (num == sorteo[i]) 
+                {
                 	repetir = true;
                     break;
                 }
@@ -155,10 +183,13 @@ public class Bingo {
         return num;
     }
  
-    public static void numAcierto(int num, int[][] carton) {
+    public static void numAcierto(int num, int[][] carton) 
+    {
  
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[i].length; j++) {
+        for (int i = 0; i < carton.length; i++) 
+        {
+            for (int j = 0; j < carton[i].length; j++) 
+            {
                 if (carton[i][j] == num) {
                 	carton[i][j] = -1;
                 }
@@ -166,9 +197,12 @@ public class Bingo {
         }
     }
  
-    private static boolean comproBingo(int[][] carton) {
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[i].length; j++) {
+    private static boolean comproBingo(int[][] carton) 
+    {
+        for (int i = 0; i < carton.length; i++) 
+        {
+            for (int j = 0; j < carton[i].length; j++) 
+            {
                 if (carton[i][j] > 0) {
                     return false;
                 }
